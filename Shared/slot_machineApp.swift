4d768e77fdd68ee911a3c8396e4538e5a01737fa
Akshay Jadhav <akshayjadhav461999@gmail.com>
2022-09-11 14:53:06 +0530

    Initial Commit

---
+++ Shared/slot_machineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_machineApp.swift
+//  Shared
+//
+//  Created by Akshay Anil Jadhav on 11/09/22.
+//
+
+import SwiftUI
+
+@main
+struct slot_machineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
